d41ad652bcbc55941941f01af2f5d12bc20647cf
Shuaib-Ahmed <shuaibahmed8471@gmail.com>
2022-05-28 21:56:42 +0530

    updated package json

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      "mongodb+srv://shuaib:Saif123@articledb.mzhlh.mongodb.net/articleDB?retryWrites=true&w=majority"
+    );
 
     console.log("database connection made");
 
